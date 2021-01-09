6851779580eef53b97ed78ea433efd3ec0020cf5
Tan Yi Hao <yi_hao_tan@hotmail.com>
2021-01-09 10:12:03 +0800

    building for prod

---
+++ build/precache-manifest.42b9e8959998bf6612d7ad31bdf4da44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7010be770c6faa288273b68199ebda1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ece9ea9d28a18a55275b",
+    "url": "/static/css/2.a1a68d4e.chunk.css"
+  },
+  {
+    "revision": "132d96b598bd361efadc",
+    "url": "/static/css/main.f9bf26d9.chunk.css"
+  },
+  {
+    "revision": "ece9ea9d28a18a55275b",
+    "url": "/static/js/2.8bdbf57d.chunk.js"
+  },
+  {
+    "revision": "c4381524cae000488e2fb889012466d0",
+    "url": "/static/js/2.8bdbf57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132d96b598bd361efadc",
+    "url": "/static/js/main.27afb633.chunk.js"
+  },
+  {
+    "revision": "d47cadacb1d96a3010db",
+    "url": "/static/js/runtime-main.e49ae51d.js"
+  }
+]);
